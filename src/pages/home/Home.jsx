6c1d1b69db017344a9029e7c6c341eb6321db4f2
Rabyn-Collab <babynshrestha64@gmail.com>
2025-03-25 08:14:20 +0545

    hooks

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Intro from './Intro'
 import Tech from './Tech'
 import Info from './Info'
 
+//somes
 export default function Home() {
   return (
     <div className='p-4 max-sm:bg-amber-600'>
