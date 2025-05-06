2f547b369dcb72bc0e8253b152acefcee2fdd186
Rabyn-Collab <babynshrestha64@gmail.com>
2025-05-06 08:52:50 +0545

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import { NavLink } from 'react-router'
+import React from "react";
+import {
+  Navbar,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+import ProfileMenu from "./ProfileMenu";
+
+
+
+
+
 
 export default function Header() {
+
+
+
   return (
-    <div className='bg-black text-white px-5 py-2 flex items-center justify-between'>
+    <Navbar className=" p-2 ">
+      <div className="relative mx-auto flex items-center justify-between text-blue-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          className="mr-4 ml-2 cursor-pointer py-1.5 font-medium"
+        >
+          Material Tailwind
+        </Typography>
 
-      <h1>ShopiFy</h1>
-
-      <nav>
 
 
+        <Button size="sm" variant="text">
+          <span>Log In</span>
+        </Button>
+        <ProfileMenu />
+      </div>
 
-      </nav>
-
-    </div>
-  )
+    </Navbar>
+  );
 }
