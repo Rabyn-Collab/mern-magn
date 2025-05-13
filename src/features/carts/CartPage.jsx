11e8e9169aa377f49c4f24b70be3d54415972fc6
Rabyn-Collab <babynshrestha64@gmail.com>
2025-05-13 09:56:15 +0545

    changes

--- src/features/carts/CartPage.jsx
+++ src/features/carts/CartPage.jsx
@@ -4,34 +4,6 @@ import { baseUrl } from "../../app/mainApi";
 import { setToCart } from "./cartSlice";
 const TABLE_HEAD = ["Items", "Price", "Quantity", "Total"];
 
-const TABLE_ROWS = [
-  {
-    name: "John Michael",
-    job: "Manager",
-    date: "23/04/18",
-  },
-  {
-    name: "Alexa Liras",
-    job: "Developer",
-    date: "23/04/18",
-  },
-  {
-    name: "Laurent Perrier",
-    job: "Executive",
-    date: "19/09/17",
-  },
-  {
-    name: "Michael Levi",
-    job: "Developer",
-    date: "24/12/08",
-  },
-  {
-    name: "Richard Gran",
-    job: "Manager",
-    date: "04/10/21",
-  },
-];
-
 export default function CartPage() {
   const { carts } = useSelector((state) => state.cartSlice);
   return (
