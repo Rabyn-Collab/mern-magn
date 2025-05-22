6cce75d78e165907bde66cac07ee694b560f6517
Rabyn-Collab <babynshrestha64@gmail.com>
2025-05-22 09:36:50 +0545

    chnages

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -2,13 +2,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 
 //
-export const baseUrl = 'http://192.168.194.1:5000';
-//export const baseUrl = 'https://mern-magn.onrender.com';
+//export const baseUrl = 'http://192.168.194.1:5000';
+export const baseUrl = 'https://mern-magn.onrender.com';
 
 
 
 export const mainApi = createApi({
   reducerPath: 'mainApi',
-  baseQuery: fetchBaseQuery({ baseUrl: `${baseUrl}/api` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${baseUrl}/api`, credentials: 'include' }),
   endpoints: (builder) => ({})
 });
